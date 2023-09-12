9b5953ce7704fdcfc5465a5a503dc291e32a8d47
anialiulka <116043828+anialiulka@users.noreply.github.com>
2023-09-12 18:05:11 +0000

    Deploying to gh-pages from @ anialiulka/goit-react-hw-08-phonebook@15dc9377e6ec6e700e57376e36e76590288c5497 🚀

---
+++ static/js/983.09c85791.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{983:function(e,t,c){c.r(t);var r=c(184);t.default=function(){return(0,r.jsx)("div",{children:"Welcome to contacts!"})}}}]);
+//# sourceMappingURL=983.09c85791.chunk.js.map
