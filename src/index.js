247676fc025da72c4c61e5b816fe3df42255fae4
ania.liulka <anialiulka@gmail.com>
2023-09-14 16:54:48 -0400

    Fixed the 404 issue

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <Router basename="/goit-react-hw-08-phonebook">
         <App />
-      </BrowserRouter>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
