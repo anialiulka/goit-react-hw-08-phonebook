e83291833e123d3c473c23d64e1ce0368659b931
ania.liulka <anialiulka@gmail.com>
2023-09-14 17:14:47 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router basename="/goit-react-hw-08-phonebook">
+      <Router>
         <App />
       </Router>
     </Provider>
